916bb210ad18c64683bd06a0318f0c0504d57e9f
JON95Git <jonathan.s.gonzaga@gmail.com>
2021-10-31 20:10:40 -0300

    Implements get_rng function

--- app/rng_user/rng_user.c
+++ app/rng_user/rng_user.c
@@ -1,6 +1,19 @@
 #include "rng_user.h"
+#include "portable/portable.h"
 
 void init_rng_user(void)
 {
 
+}
+
+uint8_t get_rng(uint8_t *buff, uint16_t buff_len)
+{
+    uint8_t ret = 0;
+    for (uint16_t i = 0; i < buff_len; i++) {
+        ret = _get_rng(&buff[i]);
+        if (ret != 0) {
+            return ret;
+        }
+    }
+    return ret;
 }
--- app/rng_user/rng_user.h
+++ app/rng_user/rng_user.h
@@ -1,6 +1,9 @@
 #ifndef __RNG_USER_H
 #define __RNG_USER_H
 
+#include <stdint.h>
+
 void init_rng_user(void);
+uint8_t get_rng(uint8_t *buff, uint16_t buff_len);
 
 #endif /* __RNG_USER_H */
